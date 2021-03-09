828861d27af1300eec1822c2450eb79fa2e513da
dr-chene <3286586983@qq.com>
2021-03-09 16:45:28 +0800

    update:move BaseBindingAdapter to base package

--- app/src/main/java/com/bee/leetcode/MainActivityAdapter.java
+++ app/src/main/java/com/bee/leetcode/MainActivityAdapter.java
@@ -6,8 +6,12 @@ import android.view.ViewGroup;
 
 import androidx.databinding.DataBindingUtil;
 import androidx.databinding.ViewDataBinding;
+
+import com.bee.leetcode.base.BaseBindingAdapter;
+
 //使用BaseBindingAdapter的举列子
-public class MainActivityAdapter extends BaseBindingAdapter<ShowDataBeans,BaseBindingAdapter.ViewHolder>{
+//ShowDataBeans显示找不到，不过以为是示例所以没影响 --yc
+public class MainActivityAdapter extends BaseBindingAdapter<ShowDataBeans,BaseBindingAdapter.ViewHolder> {
     public MainActivityAdapter(Context context){
         super(context);
     }
